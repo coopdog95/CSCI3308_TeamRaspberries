e25006a58d28c4fa74c04c05dde43e452321e9a8
liamkolber <liamkolber62@gmail.com>
2018-04-12 15:06:48 -0600

    cleaning up some junk

--- frontend (Whitespace Conflict)/dynamicPlot.js
+++ frontend (Whitespace Conflict)/dynamicPlot.js
@@ -1,5 +1,7 @@
 window.onload = function () {
-  var dataPoints = [{y : 10}];
+  var dataPoints1 = [{y : 10}];
+  var dataPoints2 = [{y : 9}];
+  var dataPoints3 = [{y : 11}];
   var chart = new CanvasJS.Chart("chartContainer", {
       title : {
         text: "DATA F@#K YEAH" //CHANGE THIS
@@ -28,28 +30,47 @@ window.onload = function () {
         name: "Sensor 1",
         markerType: "square",
         //xValueFormatString: "DD MMM, YYYY",
-        color: "#F08080",
-        dataPoints : dataPoints
+        color: "#DD0000",
+        dataPoints : dataPoints1
+      },{
+        type: "line",
+        showInLegend: true,
+        name: "Sensor 2",
+        markerType: "square",
+        //xValueFormatString: "DD MMM, YYYY",
+        color: "#00BB00",
+        dataPoints : dataPoints2
+      },{
+        type: "line",
+        showInLegend: true,
+        name: "Sensor 3",
+        markerType: "square",
+        //xValueFormatString: "DD MMM, YYYY",
+        color: "#0000BB",
+        dataPoints : dataPoints3
       }]
     });
 
   chart.render();
   
-  var yVal = 15;
+  var yVal1 = 15;
+  var yVal2 = 15;
+  var yVal3 = 15;
   var updateChart = function () {
-    yVal = yVal + Math.round(5 + Math.random() * (-5 - 5));
+    yVal1 = yVal1 + Math.round(5 + Math.random() * (-5 - 5));
+    dataPoints1.push({y : yVal1});
+    yVal2 = yVal2 + Math.round(5 + Math.random() * (-5 - 5));
+    dataPoints2.push({y : yVal2});
+    yVal3 = yVal3 + Math.round(5 + Math.random() * (-5 - 5));
+    dataPoints3.push({y : yVal3});  
+
     var d = new Date();
-    var month = d.getMonth();
-    var day = d.getDate();
-    var year = d.getFullYear();
-    var hour = d.getHours();
+    var month = (d.getMonth() < '10') ? ('0' + d.getMonth()) : d.getMonth();
+    var day = (d.getDate() < '10') ? ('0' + d.getDate()) : d.getDate();
+    var year = (d.getFullYear() < '10') ? ('0' + d.getFullYear()) : d.getFullYear();
+    var hour = (d.getHours() < '10') ? ('0' + d.getHours()) : d.getHours();
     var min = (d.getMinutes() < '10') ? ('0' + d.getMinutes()) : d.getMinutes();
-    var sec = (d.getSeconds() < '10') ? ('0' + d.getSeconds()) : d.getSeconds();
-    //var time = d.getTime();
-    
-    //Append to the dataPoints array
-    dataPoints.push({y : yVal}); 
-        
+    var sec = (d.getSeconds() < '10') ? ('0' + d.getSeconds()) : d.getSeconds();    
     chart.options.title.text = "Data as of:   " + month + "-" + day + "-" + year + " @ " + hour + ":" + min + ":" + sec;
     chart.render();    
     
